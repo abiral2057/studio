d6c29e7e85b437d763836c80c95256e48c97be64
Abiral Shrestha <abiral.shrestha72@gmail.com>
2025-09-13 18:10:24 +0000

    internal server error ?

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,18 +1,15 @@
-import { EditProfileSheet } from "@/components/settings/edit-profile-sheet";
+
 import { SettingsClient } from "@/components/settings/settings-client";
 import { getSession } from "@/lib/auth-actions";
 import { redirect } from "next/navigation";
 
 export default async function SettingsPage() {
   const session = await getSession();
-  if (!session) {
+  if (!session || !session.user) {
     redirect('/login?from=/settings');
   }
   
   return (
-    <>
-      <SettingsClient user={session.user} />
-      <EditProfileSheet user={session.user} />
-    </>
+    <SettingsClient user={session.user} />
   );
 }
